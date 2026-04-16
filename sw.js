1227f1f486dc0e0407586074f265d28926d08aee
Heureka <3455545938@qq.com>
2026-04-16 22:16:28 +0800

    Site updated: 2026-04-16 22:16:26

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1776341622361";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1776348934558";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
