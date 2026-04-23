d2eb5b1199e0f5d14ddb9f6c103db4721b49ec99
Heureka <3455545938@qq.com>
2026-04-23 13:53:56 +0800

    Site updated: 2026-04-23 13:53:55

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1776437040185";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1776923628842";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
